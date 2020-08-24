7d918b6de1793b2fa5cd7bae98179d19528f6103
Muhammad Nurhafiz <kurkurzz@users.noreply.github.com>
2020-08-24 23:18:54 +0800

    The Initial Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Instagram Profile Bomber
+Bomb any profile you like the easiest way.
+
+## Download Link
+https://github.com/kurkurzz/Instagram-Profile-Bomber/raw/master/dist/Instagram%20Profile%20Bomber.exe
+
+## How To Use:
+1. Run the application 'Instagram Profile Bomber'. <br>
+If antivirus warns you about this 'risky' app, just whitelist the application. Don't worry, there is no virus, can see the source code yourself.
+
+    In case of Windows Defender
+    1. Click 'More Info'
+
+        ![moreinfo](https://user-images.githubusercontent.com/64152220/91061343-a6531b00-e65e-11ea-811a-1055e7ac300a.jpg)
+
+    1. Click 'Run Anyway'
+
+        ![runanyway](https://user-images.githubusercontent.com/64152220/91061765-c1be2600-e65e-11ea-8164-31ad413481fe.jpg)
+
+1. Terminal will appear. <br>
+Answer the first question "Login using Facebook?" with 'y' for yes and 'n' for No.
+
+1. Insert your Instagram email and password. <br>
+Again, don't worry I will not save the credentials anywhere, can see the source code yourself.
+
+1. Insert THE Instagram profile username that you want to bomb.
+
+1. Chrome window will open and let it be, you also can let it run on the background.
+
+1. DONE!
+
+## Down side
+1. Cannot do on private profile that you are not followed yet. (obviously)
+
+1. The browser must be open full screen all the time during the process.
+
+1. It can only handle profile with 40 post maximum. If profile with posts over 40, it will just like the latest 40 post.
+
+As far as I tried, there will be no risk of banning or temporary banning as long as you are not using it excessively.
+<br>
+<br>
+Happy Bombing!
+
---
+++ main.py
@@ -0,0 +1,174 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+import os
+
+logInFacebook = False
+
+LOGINFB_XPath = '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[5]/button/span[2]'
+FBEMAILFORM_XPath = '/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[1]/input'
+FBPASSWORDFORM_XPath = '/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[2]/input'
+FBLOGINBUTTON_XPath = '/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[3]/button'
+
+EMAIL_XPath = '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input'
+PASSWORD_XPath = '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input'
+LOGINBUTTON_XPath = '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button'
+REJECTLOGININFO_XPath = '/html/body/div[1]/section/main/div/div/div/div/button'
+
+NOTNOW_XPath = '/html/body/div[4]/div/div/div/div[3]/button[2]'
+USERSEARCHBOX_XPath = '/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input'
+PICKFIRSTUSER_XPath = '/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[3]/div[2]/div/a[1]/div'
+INSTAGRID_XPath = '/html/body/div[1]/section/main/div/div[3]/article/div'
+IMAGEPOP_XPath = '/html/body/div[4]/div[3]/button/div'
+INSTAIMG_XPath = '/html/body/div[1]/section/main/div/div[3]/article/div/div/div[{}]/div[{}]'
+
+altINSTAIMG_XPath = '//*[@id="react-root"]/section/main/div/div[contains(@class," _2z6nI")]/article/div/div/div[{}]/div[{}]'
+POSTNUMBER_XPath = '//*[@id="react-root"]/section/main/div/header/section/ul/li[1]/span/span'
+LIKE_XPath = '/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button'
+
+print('\nInstagram profile Bomber\n')
+print('Please make sure the chrome window is maximise at all time.')
+while(True):
+    logInFacebooks = input("Login using Facebook?(y/n): ")
+    if logInFacebooks=='y' or logInFacebooks=='Y':
+        logInFacebook = True
+        break
+    elif logInFacebooks=='n' or logInFacebooks=='N':
+        logInFacebook = False
+        break
+    else:
+        print('Input not clear. Please try again.')   
+email = input("Please enter your Instagram email: ")
+password = input("Please enter the Instagram password: ")
+igusername = input("Please enter the the profile's username: ")
+succeedLogin = False
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.dirname(__file__)
+    return os.path.join(base_path, relative_path)
+
+driver = webdriver.Chrome(resource_path('./drivers/chromedriver.exe'))
+driver.maximize_window()
+driver.get('https://www.instagram.com')
+
+if logInFacebook:
+    try:
+        print('-Logging in with Facebook')
+        #click login with fb button
+        element = WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.XPATH,LOGINFB_XPath))
+        )
+        element.click()
+        #fill in fb email
+        element = WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.XPATH,FBEMAILFORM_XPath))
+        )
+        element.send_keys(email)
+        #fill in fb password
+        element = WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.XPATH,FBPASSWORDFORM_XPath))
+        )
+        element.send_keys(password)
+        #click login 
+        element = WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.XPATH,FBLOGINBUTTON_XPath))
+        )
+        element.click()
+        element = WebDriverWait(driver,10).until(
+            EC.presence_of_element_located((By.XPATH,NOTNOW_XPath))
+        )
+        element.click()
+        succeedLogin = True
+        print('-Succeed login with Facebook.')
+    except:
+        print('-Failed login with Facebook.')
+else:
+    try:
+        print('-Loggin in with email and password')
+        element = WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.XPATH,EMAIL_XPath))
+        )
+        element.send_keys(email)
+        element = driver.find_element_by_xpath(PASSWORD_XPath)
+        element.send_keys(password)
+        element = driver.find_element_by_xpath(LOGINBUTTON_XPath)
+        element.click()
+        element = WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.XPATH,REJECTLOGININFO_XPath))
+        )
+        element.click()
+        succeedLogin = True
+        print('-Succeed login with email and password.')   
+        element = WebDriverWait(driver,10).until(
+            EC.presence_of_element_located((By.XPATH,NOTNOW_XPath))
+        )
+        element.click()
+    except:
+        print('-Failed login with email and password.')   
+
+if succeedLogin:
+    try:       
+        element = WebDriverWait(driver,10).until(
+            EC.presence_of_element_located((By.XPATH,USERSEARCHBOX_XPath))
+        )
+        element.send_keys(igusername)
+        element = WebDriverWait(driver,10).until(
+            EC.presence_of_element_located((By.XPATH,PICKFIRSTUSER_XPath))
+        )
+        element.click()
+        element = WebDriverWait(driver,10).until(
+            EC.presence_of_element_located((By.XPATH,INSTAGRID_XPath))
+        )
+        element = driver.find_element_by_xpath(POSTNUMBER_XPath)
+        postNumber = int(element.text)
+        print('The profile contains {} posts'.format(postNumber))
+        totalRow = postNumber/3 + (postNumber%3 > 0)
+        isBreaked = False
+        column = 0
+        row = 0
+        for row in range(1,int(totalRow)+1):
+            if isBreaked:
+                break
+            for column in range(1,4):
+                image = WebDriverWait(driver,10).until(
+                    EC.presence_of_element_located((By.XPATH,altINSTAIMG_XPath.format(row ,column)))
+                )
+                # image.location_once_scrolled_into_view
+                image.click()
+                print('Post {}'.format((row-1)*3 + column),end = '')
+                try:
+                    likeButton = WebDriverWait(driver,10).until(
+                        EC.presence_of_element_located((By.XPATH,LIKE_XPath))
+                    )
+                    liked = str(likeButton.find_element_by_css_selector('svg').get_attribute("aria-label"))
+                    if liked=='Like':
+                        likeButton.click()
+                        print(' - Pressed Like')
+                    else:
+                        print('')
+                except:
+                    print(' - Problem liking')   
+                finally:     
+                    popout = driver.find_element_by_xpath(IMAGEPOP_XPath)
+                    popout.click()
+                    if (row-1)*3+column == postNumber:
+                        isBreaked = True
+                        break   
+        print('-Done bombing!')            
+    except:
+        print('')
+        print('The program counter an error, please try again.')
+    finally: 
+        driver.quit()
+else:
+    driver.quit()
+    print('')
+    print('Please try again by running the script again.')  
+
